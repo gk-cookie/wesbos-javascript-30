4dd9902e71ff419cca1b65b5b9374a693dedaf24
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-23 15:35:34 +0100

    drawing on the canvas is working

--- 8-html5-canvas/html5-canvas.js
+++ 8-html5-canvas/html5-canvas.js
@@ -5,6 +5,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = "10";
 
 let isDrawing = false;
 let lastX = 0;
@@ -13,9 +14,20 @@ let lastY = 0;
 function draw(e) {
   if (!isDrawing) return; // stops the fn from running when mouse not clicked
   console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  //   lastX = e.offsetX;
+  //   lastY = e.offsetY;
+  // can destructure like these 2 to below
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
