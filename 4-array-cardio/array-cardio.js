0380267f8ceb0ea46584f4d95d196b339c1e9834
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-27 12:33:35 +0100

    reduce and sort

--- 4-array-cardio/array-cardio.js
+++ 4-array-cardio/array-cardio.js
@@ -94,8 +94,34 @@ console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+// var yearsLived = 0;
+
+// for(var i = 0; i < inventors.length; i++) {
+//     yearsLived += inventors[i].year
+// }
+
+// console.log(yearsLived);
+
+const yearsLived = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(yearsLived);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort(function(a, b) {
+const lastVent = a.passed - a.year;
+const nextVent = b.passed - b.year;
+return lastVent > nextVent ? -1 : 1;
+// if(lastVent > nextVent) {
+//     return -1;
+//     } else {
+//         return 1;
+//     }
+
+})
+
+console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
