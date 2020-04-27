e85ea17c66485479af12d640f76dd58cd40ff2e4
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-27 13:59:47 +0100

    reduce with data, finished exercise

--- 4-array-cardio/array-cardio.js
+++ 4-array-cardio/array-cardio.js
@@ -164,3 +164,14 @@ const data = [
   "car",
   "truck",
 ];
+
+const transport = data.reduce(function (obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transport);
+
