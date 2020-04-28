36d746383d704a297ef25782a5089bac8a06cb43
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-28 10:15:10 +0100

    looping over each checkbox, checking if checked

--- 10-hold-shift-checkboxes/shift-checkboxes.js
+++ 10-hold-shift-checkboxes/shift-checkboxes.js
@@ -1,8 +1,19 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 console.log(checkboxes);
 
+let lastChecked;
+
 function handleCheck(e) {
-  console.log(e);
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+    });
+  }
+  lastChecked = this;
 }
 
 checkboxes.forEach((checkbox) =>
