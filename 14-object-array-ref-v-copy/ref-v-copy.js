66318f9073ca3e4afb6066cf826203cf79c4c0a5
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-29 14:42:51 +0100

    object references

--- 14-object-array-ref-v-copy/ref-v-copy.js
+++ 14-object-array-ref-v-copy/ref-v-copy.js
@@ -5,40 +5,31 @@ console.log(age, age2);
 age = 200;
 console.log(age, age2);
 
-let name = 'Cookie';
-let name2 = name; 
+let name = "Cookie";
+let name2 = name;
 console.log(name, name2);
-name = 'Cookie K'
+name = "Cookie K";
 console.log(name, name2);
-
 
 // Let's say we have an array
 const players = ["Cook", "Mike", "Martin", "Brian"];
-
 
 // and we want to make a copy of it.
 const team = players;
 console.log(players, team);
 // team[3] = 'Joey';
 
-
-// You might think we can just do something like this:
-
-// however what happens when we update that array?
-
-// now here is the problem!
-
-// oh no - we have edited the original array too!
-
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
 
 const team2 = players.slice();
-team2[3] = 'Joey';
+team2[3] = "Joey";
 console.log(team2, players);
 
-
+const team3 = [].concat(players);
+const team4 = [...players];
+const team5 = Array.from(players);
 // one way
 
 // or create a new array and concat the old one in
@@ -57,8 +48,36 @@ const person = {
 
 // and think we make a copy:
 
+const captain = person;
+captain.number = 99;
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99})
+console.log(cap2);
+
 
 // We will hopefully soon see the object ...spread
+// const cap3 = {...person} should be available soon
 
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+const gerard = {
+    name: 'Gerard',
+    age: 68,
+    social: {
+        twitter: '@gerryk',
+        facebook: 'gerry.k'
+    }
+}
+
+console.log(gerard);
+
+const ger = Object.assign({}, gerard);
+ger.name = 'Gerry';
+console.log(ger);
+console.log(gerard);
+// only goes down one level, if social is changed it will change in gerard and ger
+
+
+
+
+
