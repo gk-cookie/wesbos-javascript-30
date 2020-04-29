6e9ba65dcd619c5bab39d674ed701e3ca9b86a89
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-29 11:41:58 +0100

    added debounce to checkSlider fn, if state slides

--- 13-slide-in-on-scroll/slide-scroll.js
+++ 13-slide-in-on-scroll/slide-scroll.js
@@ -1,23 +1,38 @@
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  }
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-  const sliderImages = document.querySelectorAll('.slide-in');
+const sliderImages = document.querySelectorAll(".slide-in");
 
-  function checkSlide(e) {
-      console.log(e);
-      
-  }
+function checkSlide(e) {
+  sliderImages.forEach((sliderImage) => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // halfway through the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    //  bottom of the image
 
-  window.addEventListener('scroll', checkSlide);
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPassed = window.scrollY < imageBottom;
+   
+    if (isHalfShown && isNotScrolledPassed) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
