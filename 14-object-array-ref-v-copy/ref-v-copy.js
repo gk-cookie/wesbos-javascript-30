89d5159da91bb286b67b53b3b1469a64b4aaebef
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-29 14:30:55 +0100

    copying array

--- 14-object-array-ref-v-copy/ref-v-copy.js
+++ 14-object-array-ref-v-copy/ref-v-copy.js
@@ -1,42 +1,64 @@
 // start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
 
-    // Let's say we have an array
-    const players = ['Cook', 'Mike', 'Martin', 'Brian'];
+let name = 'Cookie';
+let name2 = name; 
+console.log(name, name2);
+name = 'Cookie K'
+console.log(name, name2);
 
-    // and we want to make a copy of it.
 
-    // You might think we can just do something like this:
+// Let's say we have an array
+const players = ["Cook", "Mike", "Martin", "Brian"];
 
-    // however what happens when we update that array?
 
-    // now here is the problem!
+// and we want to make a copy of it.
+const team = players;
+console.log(players, team);
+// team[3] = 'Joey';
 
-    // oh no - we have edited the original array too!
 
-    // Why? It's because that is an array reference, not an array copy. They both point to the same array!
+// You might think we can just do something like this:
 
-    // So, how do we fix this? We take a copy instead!
+// however what happens when we update that array?
 
-    // one way
+// now here is the problem!
 
-    // or create a new array and concat the old one in
+// oh no - we have edited the original array too!
 
-    // or use the new ES6 Spread
+// Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
-    // now when we update it, the original one isn't changed
+// So, how do we fix this? We take a copy instead!
 
-    // The same thing goes for objects, let's say we have a person object
+const team2 = players.slice();
+team2[3] = 'Joey';
+console.log(team2, players);
 
-    // with Objects
-    const person = {
-      name: 'Gerard Kelly',
-      age: 80
-    };
 
-    // and think we make a copy:
+// one way
 
-    // how do we take a copy instead?
+// or create a new array and concat the old one in
 
-    // We will hopefully soon see the object ...spread
+// or use the new ES6 Spread
 
-    // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+// now when we update it, the original one isn't changed
+
+// The same thing goes for objects, let's say we have a person object
+
+// with Objects
+const person = {
+  name: "Gerard Kelly",
+  age: 80,
+};
+
+// and think we make a copy:
+
+// how do we take a copy instead?
+
+// We will hopefully soon see the object ...spread
+
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
