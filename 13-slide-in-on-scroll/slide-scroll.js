5abe59b9f356979828982b8159558c2072149d6f
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-29 11:16:18 +0100

    debounce function, selected images

--- 13-slide-in-on-scroll/slide-scroll.js
+++ 13-slide-in-on-scroll/slide-scroll.js
@@ -1,1 +1,23 @@
-console.log('working');
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  const sliderImages = document.querySelectorAll('.slide-in');
+
+  function checkSlide(e) {
+      console.log(e);
+      
+  }
+
+  window.addEventListener('scroll', checkSlide);
