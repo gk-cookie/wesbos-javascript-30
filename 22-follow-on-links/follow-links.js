6852a4863e08e6cc6d85a8ac2aff89edc70081b1
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-05-01 15:52:56 +0100

    selected emenu elements and got the rect coords

---
+++ 22-follow-on-links/follow-links.js
@@ -0,0 +1,12 @@
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+highlight.classList.add("highlight");
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  
+}
+
+triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
